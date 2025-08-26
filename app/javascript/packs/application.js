77926ee3965e2bb85e80ace50c10d1b3c500f73d
EC2 Default User <ec2-user@ip-172-31-37-62.ap-northeast-1.compute.internal>
2025-08-26 09:25:08 +0000

    2st

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,11 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+Turbolinks.start()
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+import Turbolinks from "turbolinks"
 Rails.start()
 ActiveStorage.start()
 import "bootstrap"
